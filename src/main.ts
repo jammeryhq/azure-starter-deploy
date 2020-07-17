161e1dff0768441c280537d2e3359ad0bf5616d1
Travis Reynolds <travis@cirruscreative.co.uk>
2020-07-17 14:36:04 +0000

    Use ENV's

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,17 @@ import { BlobServiceClient } from '@azure/storage-blob'
 import fs from 'fs/promises'
 import { read } from 'readdir'
 
-async function run (): Promise<void> {
-  const AZURE_STORAGE_CONNECTION_STRING = core.getInput('connection-string')
-  const AZURE_STORAGE_CONTAINER_NAME = core.getInput('container-name')
+const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING as string
+const AZURE_STORAGE_CONTAINER_NAME = process.env.AZURE_STORAGE_CONTAINER_STARTERS as string
 
+async function run (): Promise<void> {
   const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING)
   const containerClient = blobServiceClient.getContainerClient(AZURE_STORAGE_CONTAINER_NAME)
+  const blockBlobClient = containerClient.getBlockBlobClient('some')
 
   try {
-    console.log(AZURE_STORAGE_CONNECTION_STRING, AZURE_STORAGE_CONTAINER_NAME)
+    core.debug(AZURE_STORAGE_CONTAINER_NAME)
     const files = await read('.', ['*'])
-
     core.debug(JSON.stringify(files))
 
     core.debug(new Date().toTimeString())
