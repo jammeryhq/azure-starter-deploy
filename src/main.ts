b4a2cf7736f67de660ee9a912bb2a9e7f5c2b7e1
Travis Reynolds <travis@travisreynolds.dev>
2020-07-17 19:03:21 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,18 @@ import { read } from 'readdir'
 async function run (): Promise<void> {
   const AZURE_STORAGE_CONNECTION_STRING = core.getInput('connection-string')
   const AZURE_STORAGE_CONTAINER_NAME = core.getInput('container-name')
+
   const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING)
   const containerClient = blobServiceClient.getContainerClient(AZURE_STORAGE_CONTAINER_NAME)
 
   try {
+    const { name, version } = require('package.json')
     const files = await read('.')
 
+    const uploadPath = path => `${name}/${version}/${path}`
+
     for await (const path of files) {
-      const uploadPath = `/shopify/${path}`
-      const blockBlobClient = containerClient.getBlockBlobClient(uploadPath)
+      const blockBlobClient = containerClient.getBlockBlobClient(uploadPath(path))
       await blockBlobClient.uploadStream(fs.createReadStream(path))
     }
   } catch (error) {
